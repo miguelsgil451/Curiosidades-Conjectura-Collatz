a381049fb96214f8a8c145d9b5148c176277df67
loeasy68 <90572477+loeasy68@users.noreply.github.com>
2025-04-22 20:01:40 -0400

    Added C Program
    Added C Program to produce results for Collatz Conjecture

---
+++ main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int isEven(int num)
+{
+        if(num % 2 == 0)
+        {
+                return 0; // Is even
+        }
+        else
+        {
+                return 1; // Is odd
+        }
+}   
+
+int main()
+{
+        int source_number = 150;
+        int climbs = 0;
+        int decent = 0;
+        printf("Source Number: %d\n", source_number);
+        while (1)
+        {
+                if (source_number == 1)
+                {
+                        break;
+                }
+                if (isEven(source_number) == 0)
+                {
+                        source_number = source_number/2;
+                        decent+=1;
+                } else if (isEven(source_number) == 1)
+                {
+                        source_number = source_number*3;
+                        source_number = source_number+1;
+                        climbs+=1;
+                }
+        }
+        printf("Climbs: %d\n", climbs);
+        printf("Decent: %d\n", decent);
+        
+        return 0;
+}
