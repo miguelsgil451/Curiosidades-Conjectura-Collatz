9d9b51af8908481588140c2359517a6bd309f18c
loeasy68 <90572477+loeasy68@users.noreply.github.com>
2025-04-22 20:11:15 -0400

    Update main.c
    Added a loop that can go through numbers and output each climb and decent

--- main.c
+++ main.c
@@ -14,29 +14,35 @@ int isEven(int num)
 
 int main()
 {
-        int source_number = 150;
-        int climbs = 0;
-        int decent = 0;
-        printf("Source Number: %d\n", source_number);
-        while (1)
-        {
-                if (source_number == 1)
-                {
-                        break;
+        int max_number = 10000;
+        for (int i = 0; i<max_number;i++) {
+                if (i == 0) {
+                        continue;
                 }
-                if (isEven(source_number) == 0)
+                int source_number = i;
+                int climbs = 0;
+                int decent = 0;
+                printf("Source Number: %d\n", source_number);
+                while (1)
                 {
-                        source_number = source_number/2;
-                        decent+=1;
-                } else if (isEven(source_number) == 1)
-                {
-                        source_number = source_number*3;
-                        source_number = source_number+1;
-                        climbs+=1;
+                        if (source_number == 1)
+                        {
+                                break;
+                        }
+                        if (isEven(source_number) == 0)
+                        {
+                                source_number = source_number/2;
+                                decent+=1;
+                        } else if (isEven(source_number) == 1)
+                        {
+                                source_number = source_number*3;
+                                source_number = source_number+1;
+                                climbs+=1;
+                        }
                 }
+                printf("Climbs: %d\n", climbs);
+                printf("Decent: %d\n\n", decent);
         }
-        printf("Climbs: %d\n", climbs);
-        printf("Decent: %d\n", decent);
         
         return 0;
 }
